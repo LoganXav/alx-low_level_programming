d3a8824f009a26bbe37526124048d2d0c76fec7e
Sogbesan Timilehin <sogbesansegun22@gmail.com>
2022-10-15 00:45:46 -0700

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+		putchar(alp);
+	for (alp = 'A'; alp <= 'Z'; alp++)
+		putchar(alp);
+	putchar('\n');
+	return (0);
+}
